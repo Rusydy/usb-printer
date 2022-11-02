567646ee491bea77edf277aead67b369816f3518
Rusydy <rusydymuhiddin@gmail.com>
2022-11-03 03:21:41 +0700

    UPDATE: composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '467b195dcbc2cf26b17d25bca436fa87972ee511',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '467b195dcbc2cf26b17d25bca436fa87972ee511',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
